efa86817957e37d2f12f9c4e3f970a839fcb6c65
kaa <kaa@sdexport.ru>
2025-11-07 17:43:01 +0700

    Задание 1.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace Module5
     {
         static void Main(string[] args)
         {
+            //Урок 5.1
+
+            //Задание 1 - Создайте кортеж User, содержащий имя пользователя Name и массив с текстовой информацией
+            //о его пяти любимых блюдах Dishes. Заполните имя пользователя через ввод в консоль,
+            //а информацию о блюдах — в цикле через консоль.
+            (string Name, string[] Dishes) User;
+
+            Console.WriteLine("Введите имя: ");
+            User.Name = Console.ReadLine();
+
+            User.Dishes = new string[5];
+            
+            for (int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.WriteLine("Введите любимое блюдо номер {0}:", i + 1);
+                User.Dishes[i] = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+            //Конец задания 1.
         }
     }
 }
