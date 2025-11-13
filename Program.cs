5115d7fadefa0cd47d6d8145525d8772bfab1628
kaa <kaa@sdexport.ru>
2025-11-13 13:18:56 +0700

    Задание 5.1.5

--- Program.cs
+++ Program.cs
@@ -9,14 +9,13 @@ namespace Module5
     class Program
     {
         //Урок 5.1
-        //Создание метода и вызов из разных частей программы
+        //Задание 5.1.5 - Напишите программу, которая в цикле вызывает метод ShowColor(), записывает его значение в массив из трех цветов favcolors, а потом отображает значения этого массива. 
         static /*void*/ string ShowColor()
         {
             Console.WriteLine("Введите свой любимый цвет на английском с маленькой буквы");
 
             var color = Console.ReadLine();
-
-            return (color);
+            
             switch (color)
             {
                 case "red":
@@ -47,17 +46,17 @@ namespace Module5
                     Console.WriteLine("Ваш любимый цвет тот, который вы ввели.");
                     break;
             }
+            return(color);
         }
-
+        //Конец задания 5.1.5
 
         static void Main(string[] args)
         {
             //Урок 5.1
+            //Задание 5.1.5 - Напишите программу, которая в цикле вызывает метод ShowColor(), записывает его значение в массив из трех цветов favcolors, а потом отображает значения этого массива. 
             var (name, age) = ("Jarvis", 27);
-            
-            Console.WriteLine("Моё имя {0}, мой возраст {1}", name, age);
 
-            //ShowColor();
+            Console.WriteLine("Моё имя {0}, мой возраст {1}", name, age);
 
             Console.Write("Введите своё имя: ");
             name = Console.ReadLine();
@@ -65,16 +64,25 @@ namespace Module5
             age = int.Parse(Console.ReadLine());
 
             //ShowColor();
-            string color = ShowColor();
+            string[] favcolors = new string[3];
+
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor();
+            }
 
             Console.WriteLine("Ваше имя: {0}", name);
             Console.WriteLine("Ваш возраст: {0}", age);
-            Console.WriteLine("Ваш цвет: {0}", color);
-            Console.WriteLine();
+            Console.WriteLine("Ваши цвета:");
 
-            //ShowColor();
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
 
             Console.ReadKey();
+            //Конец задания 5.1.5
+
 
             //Урок 5.0
 
